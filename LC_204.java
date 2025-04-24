d22e97f5a624fabfe3eb495e14cfb108731b1bdd
Shivam Kumar <shivam.kr1021@gmail.com>
2025-04-24 12:52:56 +0530

    Add files via upload

---
+++ LC_204.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class LC_204 {
+    public static int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);  
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) { 
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i=2;i<n;i++){
+            if(isPrime[i]){
+                count++;
+            }
+        }
+
+        return count;
+    }
+    public static void main(String[] args) {
+        int n=10;
+        System.out.println(countPrimes(n));
+    }
+}
